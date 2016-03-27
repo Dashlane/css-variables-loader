da903f2ad97b220c643aebc4a0d41135a8528d74
Denis Sokolov <denis.sokolov@dashlane.com>
2016-03-27 18:03:29 +0200

    Initial import

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var css = require('css');
+module.exports = function(source) {
+  this.cacheable();
+  var result = {};
+  css.parse(source).stylesheet.rules
+    .filter(rule => rule.selectors.some(sel => sel === ':root'))
+    .forEach(rule => rule.declarations
+      .filter(decl => decl.property.indexOf('--') === 0)
+      .forEach(decl => result[decl.property] = decl.value));
+  return 'module.exports = ' + JSON.stringify(result);
+};
---
+++ test/simple.js
@@ -0,0 +1,32 @@
+/* eslint no-eval: 0 */
+var test = require('ava');
+var cssVariableLoader = require('..');
+
+var loader = cssVariableLoader.bind({ cacheable: function(){} });
+
+var check = function(name, css, result){
+    test(name, function(t){
+        var module = { exports: {} };
+        eval(loader(css));
+        t.same(module.exports, result);
+    });
+};
+
+check('should handle a simple variable',
+    ':root { --theme: red }',
+    { '--theme': 'red' })
+check('should handle multiple variables',
+    ':root { --theme: red; --accent: blue }',
+    { '--accent': 'blue', '--theme': 'red' })
+check('should handle multiple declarations',
+    ':root { --theme: red } :root { --accent: blue }',
+    { '--accent': 'blue', '--theme': 'red' })
+check('should not import non-variables',
+    ':root { color: red }',
+    {})
+check('should only import from :root',
+    ':hover { --theme: red }',
+    {})
+check('should import from :root among multiple selectors',
+    ':hover, :root { --theme: red }',
+    { '--theme': 'red' })
