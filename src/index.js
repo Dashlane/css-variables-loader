b84a536fce8fd545d2d6bb84072f72bc88532e40
Simon Andrews <simon.andrews@thomsonreuters.com>
2016-12-19 13:32:51 +0000

    Don't fail on file-level comments
    The loader works great when there are comments within a selector, but
    before this commit, if there were comments in a file that aren’t within
    a selector, they were causing the loader to fail.
    
    Such a comment doesn’t have any selectors, so this change simply checks
    to see if `rule.selectors` is truth before continuing.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = function(source) {
   this.cacheable();
   var result = {};
   css.parse(source).stylesheet.rules
-    .filter(rule => rule.selectors.some(sel => sel === ':root'))
+    .filter(rule => rule.selectors && rule.selectors.some(sel => sel === ':root'))
     .forEach(rule => rule.declarations
       .filter(decl => decl.type === 'declaration' && decl.property.indexOf('--') === 0)
       .forEach(decl => result[decl.property] = decl.value));
